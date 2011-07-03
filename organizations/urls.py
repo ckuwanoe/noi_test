cfa0340f58b96dde9198ef69e7aa25c573b30883
Corey Kuwanoe <corey.kuwanoe@gmail.com>
2011-07-02 20:57:49 -0700

    Adding detail view for Person

--- organizations/urls.py
+++ organizations/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('organizations.views',
     (r'^$', 'index'),
     (r'^(?P<organization_id>\d+)/$', 'detail'),
+    (r'^staff/(?P<person_id>\d+)/$', 'people_detail')
 )
--- organizations/views.py
+++ organizations/views.py
@@ -1,4 +1,5 @@
 from organizations.models import Organization
+from organizations.models import Person
 from django.shortcuts import render_to_response, get_object_or_404
 
 def index(request):
@@ -8,3 +9,7 @@ def index(request):
 def detail(request, organization_id):
     org = get_object_or_404(Organization, pk=organization_id)
     return render_to_response('organizations/detail.html', {'org': org})
+
+def people_detail(request, person_id):
+    p = get_object_or_404(Person, pk=person_id)
+    return render_to_response('organizations/person_detail.html', {'person': p})
